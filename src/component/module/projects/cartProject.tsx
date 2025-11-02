cbbdd5dbe33c8197a36345854d93a84733e5514d
Siam Sheikh <siamthecoder@gmail.com>
2025-11-02 20:14:20 +0600

    .

--- src/component/module/projects/cartProject.tsx
+++ src/component/module/projects/cartProject.tsx
@@ -3,7 +3,7 @@ function CartProject() {
         <div className="grid grid-rows-3 md:grid-rows-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-25">
             {/* item 1 start */}
             <div className=" rounded-2xl overflow-hidden">
-                <img src="../public/images/lending.png" alt="site-1" className="h-[45%] w-full cursor-pointer" />
+                <img src="/images/lending.png" alt="site-1" className="h-[45%] w-full cursor-pointer" />
                 <div className="bg-[#11152C] p-5 hover:border hover:border-purple-400 rounded-b-[5px]">
                     <h4 className="font-bold my-3 text-2xl">Modern Landing Page</h4>
                     <p className="text-[#9CA392]">A clean, responsive landing page designed for fast loading and smooth user experience.
@@ -22,7 +22,7 @@ function CartProject() {
 
             {/* item 2 start */}
             <div className=" rounded-2xl overflow-hidden">
-                <img src="../public/images/madrasha.png" alt="site-1" className="h-[45%] w-full cursor-pointer" />
+                <img src="/images/madrasha.png" alt="site-1" className="h-[45%] w-full cursor-pointer" />
                 <div className="bg-[#11152C] p-5 hover:border hover:border-purple-400 rounded-b-[5px]">
                     <h4 className="font-bold my-3 text-2xl">Educational Website</h4>
                     <p className="text-[#9CA392]">A full-featured school website showcasing courses, announcements, and an easy contact form.
@@ -47,7 +47,7 @@ function CartProject() {
 
             {/* item 3 start */}
             <div className=" rounded-2xl overflow-hidden">
-                <img src="../public/images/resturent.png" alt="site-1" className="h-[45%] w-full cursor-pointer" />
+                <img src="/images/resturent.png" alt="site-1" className="h-[45%] w-full cursor-pointer" />
                 <div className="bg-[#11152C] p-5 hover:border hover:border-purple-400 rounded-b-[5px]">
                     <h4 className="font-bold my-3 text-2xl">Food Ordering</h4>
                     <p className="text-[#9CA392]">A stylish restaurant site featuring menu display, online reservations, and contact details.
