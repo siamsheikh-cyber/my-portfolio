d92c5637fa6ed1218f6a72e5ac94617d35e6b966
Siam Sheikh <siamthecoder@gmail.com>
2025-11-02 20:09:06 +0600

    .

--- src/component/module/about/leftAbout.tsx
+++ src/component/module/about/leftAbout.tsx
@@ -3,7 +3,7 @@ function LeftAbout() {
     return <>
         <div className="md:w-[50%] justify-between pt-15">
             <h3 className="w-[90%] text-[#16F2B3] text-center font-bold text-[27px] mb-3">Who Am I?</h3>
-            <p className="w-[90%] text-[17px] font-[400] leading-[27px]">Hi, I'm Siam Sheikh — a dedicated and motivated Front-End Web Developer with a strong passion for building modern, responsive, and user-friendly web applications. I specialize in HTML5, Tailwind CSS, Bootstrap, JavaScript, React.js and I love turning creative ideas into real-world projects. I'm experienced with tools and libraries like React Router, Context API, and localStorage, and I'm also comfortable working with backend technologies like Node.js, Express.js, Firebase, and MongoDB for full-stack development.</p>
+            <p className="text-center w-[90%] text-[17px] font-[400] leading-[27px]">Hi, I'm Siam Sheikh — a dedicated and motivated Front-End Web Developer with a strong passion for building modern, responsive, and user-friendly web applications. I specialize in HTML5, Tailwind CSS, Bootstrap, JavaScript, React.js and I love turning creative ideas into real-world projects. I'm experienced with tools and libraries like React Router, Context API, and localStorage, and I'm also comfortable working with backend technologies like Node.js, Express.js, Firebase, and MongoDB for full-stack development.</p>
         </div>
     </>
 }
