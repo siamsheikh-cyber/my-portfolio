9a2adbf51ee34c0bb91b823b7b4a04325f0f524a
Siam Sheikh <siamthecoder@gmail.com>
2025-11-02 20:18:39 +0600

    .

--- src/component/module/experiences/leftExperience.tsx
+++ src/component/module/experiences/leftExperience.tsx
@@ -1,7 +1,7 @@
 function LeftExperience() {
     return <>
         <div className="w-[50%] hidden lg:block flex items-center justify-center">
-            <img src="../public/images/leptop.png" alt="leptop" className="bounce" />
+            <img src="/images/leptop.png" alt="leptop" className="bounce" />
         </div>
     </>
 }
