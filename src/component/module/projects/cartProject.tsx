b74ab6b34f354510009976d4f9d9b31d5e5da058
Siam Sheikh <siamthecoder@gmail.com>
2025-11-10 22:03:24 +0600

    logo

--- src/component/module/projects/cartProject.tsx
+++ src/component/module/projects/cartProject.tsx
@@ -1,17 +1,17 @@
 function CartProject() {
     const projects = [
         {
-            title: "Modern Landing Page",
-            img: "/images/lending.png",
-            desc: "A clean, responsive landing page designed for fast loading and smooth user experience. Built with Tailwind CSS to look perfect on mobile, tablet, and desktop devices.",
-            tech: ["HTML", "CSS"],
-            site: "#",
+            title: "Blog Site",
+            img: "/images/frontend-blog.png",
+            desc: "A responsive blog platform built with React.js and Tailwind CSS. It features a clean UI, smooth navigation, and fast performance, focusing on readability and user experience.",
+            tech: ["HTML", "CSS", "Tailwind", "React.js",],
+            site: "https://frontend-blog-site-qcsqwdy63-siam-sheikhs-projects-579cdcb0.vercel.app/",
         },
         {
             title: "Portfolio",
             img: "/images/exam-light.png",
             desc: "A modern and elegant personal portfolio website built with the latest web technologies. Designed with a clean UI, smooth animations, and a fully responsive layout that looks perfect on any device.",
-            tech: ["HTML", "Tailwind", "React", "React-Router"],
+            tech: ["HTML", "Tailwind", "React",],
             site: "https://protfolio-2-eight.vercel.app/",
         },
         {
@@ -26,17 +26,21 @@ function CartProject() {
     return (
         <div className="grid grid-rows-3 md:grid-rows-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-25">
             {projects.map((project, index) => (
-                <div key={index} className="rounded-2xl overflow-hidden group">
-                    <div className="relative">
+                <div
+                    key={index}
+                    className="rounded-2xl overflow-hidden group"
+                >
+                    <div className="relative h-[250px] overflow-hidden">
                         <a href={project.site} target="_blank" rel="noopener noreferrer">
                             <img
                                 src={project.img}
                                 alt={project.title}
-                                className="h-[45%] w-full cursor-pointer group-hover:scale-105 transition-transform duration-700"
+                                className="w-full h-auto transition-transform duration-[4000ms] ease-linear group-hover:-translate-y-[60%] cursor-pointer"
                             />
+
                             {/* overlay */}
-                            <div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-                                <span className="text-white text-lg font-semibold tracking-wide">
+                            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500 bg-black/30">
+                                <span className="text-gray-300 text-lg font-semibold tracking-wide">
                                     Click Here
                                 </span>
                             </div>
@@ -59,6 +63,7 @@ function CartProject() {
                         </div>
                     </div>
                 </div>
+
             ))}
         </div>
     );
