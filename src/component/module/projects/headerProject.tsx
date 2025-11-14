b9e1bbd132a2052081690cc4896a37cc903df0b9
Siam Sheikh <siamthecoder@gmail.com>
2025-11-14 19:53:55 +0600

    .

--- src/component/module/projects/headerProject.tsx
+++ src/component/module/projects/headerProject.tsx
@@ -1,6 +1,6 @@
 function HeaderProject() {
     return <>
-        <button className="borderAnimation font-bold border bg-[#211B49] px-5 py-2 text-center relative ">
+        <button className="borderAnimation font-bold border bg-[#2a235a] px-5 py-2 text-center relative ">
             PROJECTS <span className="absolute top-[20px] left-30.5 bg-white w-[150px] sm:w-[430px] md:w-[580px] lg:w-[780px] h-[1px] animated-line"></span>
         </button>
     </>
